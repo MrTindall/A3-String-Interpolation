12b5addaa10e6d17fd71d8cc7e1f2a07a91061af
Alex <tindall.al@gmail.com>
2023-09-12 16:46:59 -0500

    Finished Assignment

--- Program.cs
+++ Program.cs
@@ -17,17 +17,28 @@
             //2.2019.01.22
             Console.WriteLine($"{date:yyyy.MM.dd}");
 
+            //3.Day 22 of January, 2019
+            Console.WriteLine($"Day {date:dd} of {date:MMMM, yyyy}");
+
+            //4.Year: 2019, Month: 01, Day: 22
+            Console.WriteLine($"Year: {date:yyyy}, Month: {date:MM}, Day: {date:dd}");
+
+            //5.            Tuesday (10 spaces total, right aligned)
+            Console.WriteLine($"{date,10:dddd}");
+            //6.     11:01 PM             Tuesday (10 spaces total for each including the day-of-week, both right-aligned)
+            Console.WriteLine($"{date,10:hh:mm:tt}{date,10:dddd}");
+            //7.h:11, m:01, s:27
+            Console.WriteLine($"h:{date:hh}, m:{date:mm}, s:{date:ss}");
+            //8.2019.01.22.11.01.27
+            Console.WriteLine($"{date:yyyy.MM.hh.mm.ss}");
 
 
-
-
-
-
-
-
-
+            //1. Output as currency
             var pi = Math.PI;
+            Console.WriteLine($"{pi:C}");
 
+            //2. Output as right-aligned (10 spaces), number with 3 decimal places
+            Console.WriteLine($"{pi,10:F3}");
 
         }
     }
